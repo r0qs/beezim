071751d7d1b9701fae7f9c4f4d6192d3e88a55f0
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-03-08 01:13:43 +0100

    Fix redirections and adjust metadata

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -3,9 +3,7 @@ package indexer
 import (
 	"archive/tar"
 	"bytes"
-	"context"
 	"embed"
-	"fmt"
 	"html/template"
 	"log"
 	"os"
@@ -27,11 +25,20 @@ var assetsFS embed.FS
 //go:embed templates/*
 var templateFS embed.FS
 
+var templates *template.Template
+
+func init() {
+	var err error
+
+	templates, err = template.ParseFS(templateFS, "templates/*.html")
+	if err != nil {
+		log.Fatal("error parsing templates:", err)
+	}
+}
+
 type Article struct {
-	path     string
-	title    string
-	data     []byte
-	mimeType string
+	path string
+	data []byte
 }
 
 func (a Article) Path() string {
@@ -43,56 +50,43 @@ func (a Article) Data() []byte {
 }
 
 type SwarmWikiIndexer struct {
-	mu        sync.Mutex
-	outputDir string
-	ZimPath   string
-	Z         *zim.ZimReader
-	m         map[string]ManifestEntry // TODO: replace by a real swarm manifest?
-	uploaded  string                   // TODO: hash of the root manifest metadata (if empty, not uploaded)
-	templates *template.Template
+	mu      sync.Mutex
+	ZimPath string
+	Z       *zim.ZimReader
+	entries map[string]IndexEntry // RELATIVE_PATH or ArticleID -> METADATA ?
+	root    swarm.Address         // TODO: hash of the root manifest metadata (if empty, not uploaded)
 }
 
-// ManifestEntry abstract a swarm manifest entry for a local
-type ManifestEntry struct {
-	Reference string
-	Path      string
-	Metadata  map[string]string // TODO: define all metadata entries
+type IndexEntry struct {
+	Path     string
+	Metadata map[string]string
 }
 
-func New(zimPath string, outputDir string) (*SwarmWikiIndexer, error) {
-	// load base templates
-	templates, err := template.ParseFS(templateFS, "templates/*.html")
-	if err != nil {
-		return nil, err
-	}
-
+func New(zimPath string) (*SwarmWikiIndexer, error) {
 	z, err := zim.NewReader(zimPath, false)
 	if err != nil {
 		return nil, err
 	}
 
 	return &SwarmWikiIndexer{
-		outputDir: outputDir,
-		ZimPath:   zimPath,
-		Z:         z,
-		m:         make(map[string]ManifestEntry),
-		templates: templates,
+		ZimPath: zimPath,
+		Z:       z,
+		entries: make(map[string]IndexEntry),
 	}, nil
 }
 
-func (idx *SwarmWikiIndexer) AddEntry(entry ManifestEntry) {
+func (idx *SwarmWikiIndexer) AddEntry(entryPath string, metadata map[string]string) {
 	idx.mu.Lock()
 	defer idx.mu.Unlock()
 
-	idx.m[entry.Path] = ManifestEntry{
-		Reference: entry.Reference,
-		Path:      entry.Path,
-		Metadata:  entry.Metadata,
+	idx.entries[entryPath] = IndexEntry{
+		Path:     entryPath,
+		Metadata: metadata,
 	}
 }
 
-func (idx *SwarmWikiIndexer) Manifest() map[string]ManifestEntry {
-	return idx.m
+func (idx *SwarmWikiIndexer) Entries() map[string]IndexEntry {
+	return idx.entries
 }
 
 func (idx *SwarmWikiIndexer) ParseZIM() chan Article {
@@ -108,33 +102,53 @@ func (idx *SwarmWikiIndexer) ParseZIM() chan Article {
 		idx.Z.ListTitlesPtrIterator(func(i uint32) {
 			a, err := idx.Z.ArticleAtURLIdx(i)
 			if err != nil || a.EntryType == zim.DeletedEntry {
-				log.Fatalf("Error or deleted entry: %v - %v", err, a.EntryType)
+				return
 			}
 
 			// FIXME: for now, all namespaces are considered equal when parsing
 			// https://openzim.org/wiki/ZIM_file_format
-			// TODO: add search indexes
+			var data []byte
 			switch a.Namespace {
 			case '-', // Assets (CSS, JS, Favicon)
-				'A', // Text files (Article Format) ?
-				'I', // Media files?
+				'A', // Text files (Article Format)
+				'I', // Media files
 				'M', // ZIM Metadata
-				'X': // Search indexes?
-				data, err := a.Data()
-				if err != nil {
-					log.Fatal(err)
+				'X': // Search indexes
+
+				if a.EntryType == zim.RedirectEntry {
+					ridx, err := a.RedirectIndex()
+					if err != nil {
+						return
+					}
+					ra, err := idx.Z.ArticleAtURLIdx(ridx)
+					if err != nil {
+						return
+					}
+					data, err = buildRedirectPage(filepath.Base(ra.FullURL()))
+					if err != nil {
+						log.Fatalf("error building redirect page: %v", err)
+					}
+				} else {
+					data, err = a.Data()
+					if err != nil {
+						return
+					}
 				}
-				article := Article{
-					path:     a.FullURL(),
-					title:    a.Title,
-					data:     data,
-					mimeType: a.MimeType(),
+
+				zimArticles <- Article{
+					path: a.FullURL(),
+					data: data,
 				}
-				zimArticles <- article
+
+				// TODO: add addresses and searchable data
+				idx.AddEntry(a.FullURL(), map[string]string{
+					"Title":    a.Title,
+					"MimeType": a.MimeType(),
+				})
+
 				// TODO: For now we are ignoring some cases, but we should create "_exceptions/" directory in case of errors extracting the files like is done by the zim-tools.
 				// https://github.com/openzim/zim-tools/blob/a26a450110e9ca2ec1b20de8237a3bd382af71f5/src/zimdump.cpp#L214
 			default:
-				fmt.Println("ignoring entry:", a.FullURL())
 			}
 			progressBar.Increment()
 		})
@@ -145,51 +159,15 @@ func (idx *SwarmWikiIndexer) ParseZIM() chan Article {
 	return zimArticles
 }
 
-// makeManifest consumes zimArticles and create manifest entries
-func (idx *SwarmWikiIndexer) MakeManifest(ctx context.Context, files <-chan Article, quit chan struct{}) {
-	for a := range files {
-		buf := bytes.NewBuffer(a.data)
-		file := tarball.NewBufferFile(a.path, buf)
-		file.CalculateHash()
-		reference := swarm.NewAddress(file.Hash())
-
-		entry := ManifestEntry{
-			Reference: reference.String(),
-			Path:      a.path,
-			Metadata: map[string]string{
-				"Title":    a.title,
-				"MimeType": a.mimeType,
-				// TODO: add addresses and searchable data
-			},
-		}
-
-		idx.AddEntry(entry)
-
-		select {
-		case <-quit:
-			return
-		default:
-		}
-	}
-}
-
 // TODO: move file operations to its own package
-func (idx *SwarmWikiIndexer) TarZim(tarDir string, files <-chan Article) error {
-	_, err := os.Stat(idx.outputDir)
-	if os.IsNotExist(err) {
-		if err := os.MkdirAll(idx.outputDir, 0755); err != nil {
-			return err
-		}
-	}
-
-	tarFilename := filepath.Join(idx.outputDir, fmt.Sprintf("%s.tar", tarDir))
-	tarFile, err := os.Create(tarFilename)
+func (idx *SwarmWikiIndexer) TarZim(tarFile string, files <-chan Article) error {
+	f, err := os.Create(tarFile)
 	if err != nil {
 		return err
 	}
-	defer tarFile.Close()
+	defer f.Close()
 
-	tw := tar.NewWriter(tarFile)
+	tw := tar.NewWriter(f)
 	for file := range files {
 		hdr := &tar.Header{
 			Name: file.path,
@@ -214,12 +192,23 @@ func (idx *SwarmWikiIndexer) TarZim(tarDir string, files <-chan Article) error {
 	return nil
 }
 
-func (idx *SwarmWikiIndexer) MakeIndexPage(tarDir string) error {
-
+func buildRedirectPage(pagePath string) ([]byte, error) {
 	tmplData := map[string]interface{}{
-		"Path":     path.Base(idx.ZimPath),
+		"URL": pagePath,
+	}
+
+	var buf bytes.Buffer
+	if err := templates.ExecuteTemplate(&buf, "redirect.html", tmplData); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (idx *SwarmWikiIndexer) MakeIndexPage(tarFile string) error {
+	tmplData := map[string]interface{}{
+		"File":     path.Base(idx.ZimPath),
 		"Count":    strconv.Itoa(int(idx.Z.ArticleCount)),
-		"Manifest": idx.m,
+		"Articles": idx.entries,
 	}
 
 	mainPage, err := idx.Z.MainPage()
@@ -231,23 +220,23 @@ func (idx *SwarmWikiIndexer) MakeIndexPage(tarDir string) error {
 		tmplData["HasMainPage"] = true
 		tmplData["MainURL"] = mainPage.FullURL()
 	}
+	// TODO: handle the case where there is no main page.
+	// Use the default index and browse all articles
 
 	var buf bytes.Buffer
-	if err := idx.templates.ExecuteTemplate(&buf, "index.html", tmplData); err != nil {
+	if err := templates.ExecuteTemplate(&buf, "index.html", tmplData); err != nil {
 		return err
 	}
-	tarFile := filepath.Join(idx.outputDir, fmt.Sprintf("%s.tar", tarDir))
 	return tarball.AppendTarData(tarFile, tarball.NewBufferFile("index.html", &buf))
 }
 
-func (idx *SwarmWikiIndexer) MakeErrorPage(tarDir string) error {
-	data := map[string]interface{}{
-		"Path": path.Base(idx.ZimPath),
+func (idx *SwarmWikiIndexer) MakeErrorPage(tarFile string) error {
+	tmplData := map[string]interface{}{
+		"File": path.Base(idx.ZimPath),
 	}
 	var buf bytes.Buffer
-	if err := idx.templates.ExecuteTemplate(&buf, "error.html", data); err != nil {
+	if err := templates.ExecuteTemplate(&buf, "error.html", tmplData); err != nil {
 		return err
 	}
-	tarFile := filepath.Join(idx.outputDir, fmt.Sprintf("%s.tar", tarDir))
 	return tarball.AppendTarData(tarFile, tarball.NewBufferFile("error.html", &buf))
 }
