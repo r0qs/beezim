3ffdc75ef5e0ceb476bed9a22f2f19654e963bd6
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-03-17 17:48:20 +0100

    Adjust license clauses

--- internal/beeclient/api/api.go
+++ internal/beeclient/api/api.go
@@ -1,8 +1,14 @@
-// Copyright 2020 Ethersphere. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2020 Ethersphere.
+// Copyright 2022 Beezim Authors.
+// All rights reserved.
+// Use of this source code is originally governed by
+// BSD 3-Clause and our modifications by GPLv3
 // license that can be found in the LICENSE file.
 //
-// This code is based on the beekeeper beeclient api
+// This code is based on the beekeeper beeclient api.
+// The http client was split to its own package.
+// The bee api and debug api were modified and
+// simplified to fit the purposes of Beezim.
 
 package api
 
