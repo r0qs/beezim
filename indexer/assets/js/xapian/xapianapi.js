a92b495d975334c2eb4408d0ff6efa0948472777
Racin Nygaard <racin.nygaard@gmail.com>
2022-03-17 22:43:44 +0100

    About and get random article

--- indexer/assets/js/xapian/xapianapi.js
+++ indexer/assets/js/xapian/xapianapi.js
@@ -1,19 +1,22 @@
-/* Copyright (C) 2022  Beezim
+/*
+  Copyright (C) 2016-2018 Runbox Solutions AS (runbox.com).
+  Copyright (C) 2022 ZXS Authors
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>
 
-This file is based on Runbox 7 (Runbox Solutions AS).
+  This file is based on Runbox 7 (Runbox Solutions AS) and was modified
+  to search on Xapian indexes provided by ZIM files.
 */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.XapianAPI = void 0;
